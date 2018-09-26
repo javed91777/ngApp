5d5efcb0389f7c3dfddd9ad4521f475f6e75e02d
MS\jsyed101 <javeedali_syed@optum.com>
2018-09-26 07:26:56 +0530

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {CustomersComponent} from './customers/customers.component';
-import {customerManagerPortalNavbar} from './customerManagerPortalNavbar/customerManagerPortalNavbar.component';
+import {MenuComponent} from './core/menu.component'
+import {Routes} from "@angular/router"
+import {RouterModule} from "@angular/router"
+import {routes} from "./app.route";
 
 @NgModule({
   declarations: [
-    AppComponent, CustomersComponent, customerManagerPortalNavbar
+    AppComponent, CustomersComponent, MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.route.ts
@@ -0,0 +1,6 @@
+import {Routes} from "@angular/router";
+import {CustomersComponent} from "./customers/customers.component"
+
+export const routes:Routes=[
+    {path: 'customers', component: CustomersComponent}
+]
